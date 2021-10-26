6d7a7694762d00a19df760078531adc3acbec500
dualseason <2315363424@qq.com>
2021-10-26 18:41:20 +0800

    修改跨域

---
+++ src/main/java/edu/lingnan/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package edu.lingnan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author dualseason
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
